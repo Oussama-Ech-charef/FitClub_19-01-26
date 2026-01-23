d06ed6e055225dd4ce468978c55713f3bdb38275
OUSSAMA <oussamacharef71@gmail.com>
2026-01-23 23:09:45 +0100

    add modification to main.js

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const scrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 1000,
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
 };
 
-ScrollReveal().reveal(".header__img img",{
-    ...scrollRevealOption,
-    origin: "left",
+ScrollReveal().reveal(".header__img img", {
+  ...scrollRevealOption,
+  origin: "left",
 });
-ScrollReveal().reveal(".header__content h1",{
-    ...scrollRevealOption,
-    delay: 500,
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
 });
-ScrollReveal().reveal(".header__content .section__description",{
-    ...scrollRevealOption,
-    delay: 1000,
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
 });
-ScrollReveal().reveal(".header__links",{
-    ...scrollRevealOption,
-    delay: 1500,
+ScrollReveal().reveal(".header__links", {
+  ...scrollRevealOption,
+  delay: 1500,
 });
-
 
 const workout = document.querySelector(".workout__images");
 
@@ -40,46 +39,41 @@ ScrollReveal().reveal(".workout__content .section__description", {
   delay: 500,
 });
 
-ScrollReveal().reveal(".story__image img",{
-    ...scrollRevealOption,
-    origin: "left",
+ScrollReveal().reveal(".story__image img", {
+  ...scrollRevealOption,
+  origin: "left",
 });
-
 ScrollReveal().reveal(".story__content .section__header", {
-    ...scrollRevealOption,
-    delay: 500,
+  ...scrollRevealOption,
+  delay: 500,
 });
 ScrollReveal().reveal(".story__content .section__description", {
-    ...scrollRevealOption,
-    delay: 1000,
-    interval: 500,
+  ...scrollRevealOption,
+  delay: 1000,
+  interval: 500,
 });
-
 ScrollReveal().reveal(".story__link", {
-    ...scrollRevealOption,
-    delay: 2000,
+  ...scrollRevealOption,
+  delay: 2000,
 });
 
 ScrollReveal().reveal(".feature__grid li", {
-    ...scrollRevealOption,
-    interval: 500,
+  ...scrollRevealOption,
+  interval: 500,
 });
 
-ScrollReveal().reveal(".download__image img",{
-    ...scrollRevealOption,
-});
-ScrollReveal().reveal(".membership__content .section__header",{
-    ...scrollRevealOption,
-});
-ScrollReveal().reveal(".membership__content .section__header",{
-    ...scrollRevealOption,
-    delay: 500,
-});
-ScrollReveal().reveal(".membership__btn",{
-    ...scrollRevealOption,
-    delay: 1000,
+ScrollReveal().reveal(".download__image img", {
+  ...scrollRevealOption,
 });
 
-
-
-
+ScrollReveal().reveal(".membership__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".membership__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".membership__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
